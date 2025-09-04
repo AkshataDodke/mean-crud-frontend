4badab79cd706be47e60b13d3daae3c9f570434e
AkshataDodke <akshatadodke000@gmail.com>
2025-09-04 16:52:18 +0530

    updated env file to online backend

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:4000'
+    apiUrl: 'https://mean-crud-backend.onrender.com'
+    // apiUrl: 'http://localhost:4000'
 };
